34d9f897b106b9aad3f49cd8ed07681ef9da1521
Gronit Dhir <53506491+Grazava404@users.noreply.github.com>
2019-07-31 13:05:21 +0530

    Add files via upload

---
+++ Magic 8 ball.py
@@ -0,0 +1,49 @@
+question = input("""What is your question?
+=>  """)
+
+import random
+
+a = random.choice([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+
+if (a == 1):
+    print("It is certain.")
+elif (a == 2):
+    print("It is decidedly so.")
+elif (a == 3):
+    print("Without a doubt.")
+elif (a == 4):
+    print("Yes - definetly.")
+elif (a == 5):
+    print("You may rely on it.")
+elif (a == 6):
+    print("As i see it, yes.")
+elif (a == 7):
+    print("Most likely.")
+elif (a == 8):
+    print("Outlook good.")
+elif (a == 9):
+    print("Yes.")
+elif (a == 10):
+    print("Signs point to yes.")
+elif (a == 11):
+    print("Reply hazy, try again later")
+elif (a == 12):
+    print("Ask again later")
+elif (a == 13):
+    print("Better not tell you now.")
+elif (a == 14):
+    print("Cannot predict now.")
+elif (a == 15):
+    print("Concentrate and ask again.")
+elif (a == 16):
+    print("Don't count on it.")
+elif (a == 17):
+    print("My reply is no.")
+elif (a == 18):
+    print("My sources say no.")
+elif (a == 19):
+    print("Outlook not so good.")
+elif (a == 20):
+    print("Very doubtful.")
+    
+end =   input("")
